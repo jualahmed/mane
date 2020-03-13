6efade4437bbfa22f1110d469219c16512863cf2
jual ahmed <md.jual.ah@gmail.com>
2020-03-14 00:32:47 +0600

    home design

---
+++ application/views/inc/footer.php
@@ -0,0 +1,27 @@
+<script>
+	window.base_url="<?php echo base_url() ?>"
+</script>
+<!-- Form Script -->
+<script src="<?php echo base_url() ?>assets/home/js/ajax.js"></script>
+<script src="<?php echo base_url() ?>assets/home/js/formValidation.js"></script>
+<script src="<?php echo base_url() ?>assets/home/js/jquery-2.2.4.min.js"></script>
+<!-- Twitter handler -->
+<script  src="<?php echo base_url() ?>assets/home/js/tweetie.js"></script>
+<!-- Number Counter -->
+<script  src="<?php echo base_url() ?>assets/home/js/numscroller-1.0.js"></script>
+<!-- Bootstrap Core JavaScript -->
+<script src="<?php echo base_url() ?>assets/home/js/bootstrap.min.js"></script>
+<!-- Owl Carousel -->
+<script src="<?php echo base_url() ?>assets/home/js/owl.carousel.staff.js"></script>
+<!-- Scroll to Top -->
+<script  src="<?php echo base_url() ?>assets/home/js/scrolltopcontrol.js"></script>
+<!-- Animation JS -->
+<script src="<?php echo base_url() ?>assets/home/js/wow.min.js"></script>
+<!-- Custom JS -->
+<script src="<?php echo base_url() ?>assets/home/js/custom-index.js"></script>
+<!-- Custom JS -->
+<!-- Theme JavaScript -->
+<script src="<?php echo base_url() ?>assets/home/js/jquery.easing.min.js"></script>
+<script src="<?php echo base_url() ?>assets/home/js/grayscale.min.js"></script>
+</body>
+</html>
---
+++ application/views/inc/header.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="">
+<!-- Favicon -->
+<link rel="icon" href="assets/imgs/favicon.ico" type="image/x-icon">
+<title>Architevo Builder : Construction Services</title>
+<!-- Theme CSS -->
+<link href="<?php echo base_url() ?>assets/home/css/default.css" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha256-eZrrJcwDc/3uDhsdt61sL2oOBY362qM3lon1gyExkL0=" crossorigin="anonymous" />
+</head>
+<!--head -->
+<body  class="loading" id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+<!-- preloader-->
+<div id="preloader"></div>
